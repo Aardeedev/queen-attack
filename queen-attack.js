9e199e5c5db245841e768c34f177016b21a0bf63
Aardeedev <dave.james.adams@gmail.com>
2019-12-03 11:31:32 +0000

    Refactor board population and canAttack()

--- queen-attack.js
+++ queen-attack.js
@@ -4,34 +4,27 @@
 //
 
 export class QueenAttack {
-    constructor({ white = [0,3], black = [7,3] } = {}) {
-        if (white.join() === black.join()) {
-            throw new Error('Queens cannot share the same space')
-        }
-        this.white = white;
-        this.black = black;
+  constructor({ white = [0, 3], black = [7, 3] } = {}) {
+    if (white.join() === black.join()) {
+      throw new Error('Queens cannot share the same space');
     }
+    this.white = white;
+    this.black = black;
 
+    this.board = [...Array(8)].map(() => Array(8).fill('_'));
+    this.board[white[0]][white[1]] = 'W';
+    this.board[black[0]][black[1]] = 'B';
+  }
 
-    toString() {
-        this.board = [...Array(8)].map( (row, idx) => {
-            let newRow = Array(8).fill('_');
-            if ( idx === this.white[0]) {
-                newRow[this.white[1]] = 'W'
-            }
-            if ( idx === this.black[0]) {
-                newRow[this.black[1]] = 'B'
-            }
-            return newRow.join(' ');
-        }).join('\n') + '\n'
-        return this.board
-    }
+  toString() {
+    return this.board.map(row => row.join(' ')).join('\n') + '\n';
+  }
 
-    canAttack() {
-        const gradient = (this.white[0] - this.black[0]) / (this.black[1] - this.white[1])
-        const canTake = this.white[0] === this.black[0]
-            || this.white[1] === this.black[1]
-            || Math.abs(gradient) === 1
-        return canTake
-    }
+  canAttack() {
+    const difX = this.white[0] - this.black[0];
+    const difY = this.white[1] - this.black[1];
+    const onDiagonal = Math.abs(difX / difY) === 1;
+
+    return !difX || !difY || !!onDiagonal;
+  }
 }
